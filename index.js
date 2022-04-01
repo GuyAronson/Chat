4e134e81baab9ce1f17763d49be28b027671b3a9
GuyAronson <83305443+GuyAronson@users.noreply.github.com>
2022-04-01 14:19:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// console.log("hey");
+// class Banner extends React.Component{
+//     render(){
+//         return (
+//         <><div id="top-container"></div><div id="logo">
+//             <div id="logo-circle"></div>
+//             <h1><span>Talk</span><br />
+//                 <span>to</span><br />
+//                 <span>me</span><br />
+//                 <span>Bro</span>
+//             </h1>
+//         </div></>
+//         );
+//     }
+// }
+// function createBanner(){
+//     console.log("createBanner!");
+//     return <Banner/>;
+// }
+
+// const banner = ReactDOM.createRoot(document.getElementById("banner"));
+// banner.render(<Banner/>);
