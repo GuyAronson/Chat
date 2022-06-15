7117a1bd1a5b2b634ba81462e7033087f1155fbb
IdanZiv97 <76534508+IdanZiv97@users.noreply.github.com>
2022-06-15 21:43:26 +0300

    using view model for adding SSOT

--- java/com/example/androidchat/AddContactActivity.java
+++ java/com/example/androidchat/AddContactActivity.java
@@ -6,24 +6,40 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.androidchat.entities.Partner;
+import com.example.androidchat.viewmodels.ContactsViewModel;
+import com.example.androidchat.viewmodels.ContactsViewModelFactory;
 
 public class AddContactActivity extends AppCompatActivity {
     private String loggedUser;
+    private ContactsViewModel contactsVM;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
         Intent i = getIntent();
         this.loggedUser = i.getStringExtra("username");
-        // need to create ref to repo
-
+        this.contactsVM = new ViewModelProvider(this,
+                new ContactsViewModelFactory(this.loggedUser, getApplicationContext()))
+                .get(ContactsViewModel.class);
         Button addBtn = findViewById(R.id.add_submitBtn);
         addBtn.setOnClickListener( view -> {
             EditText partnerName = findViewById(R.id.add_Username);
             EditText partnerNickname = findViewById(R.id.add_Nickname);
             EditText partnerAddress = findViewById(R.id.add_Server);
-            // logic to validate partner send - handle error and start again
-            // logic to add partner and chat accordingly
+            String name = partnerName.getText().toString();
+            String address = partnerAddress.getText().toString();
+            String nickname;
+            if (!partnerNickname.getText().toString().isEmpty()) {
+                nickname = partnerNickname.getText().toString();
+            } else {
+                nickname = null;
+            }
+            Partner p = new Partner(name,nickname, address);
+            contactsVM.add(p);
             finish();
         });
 
